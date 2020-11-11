02f37a136b83e5eab5031157d6e4c349ef0e1df9
Luis Eduardo Otálora Cubides <leotalorac@unal.edu.co>
2020-11-10 23:57:10 -0500

    Update resolver.js

--- src/westudy/MS-LDAP/resolver.js
+++ src/westudy/MS-LDAP/resolver.js
@@ -6,12 +6,12 @@ const URL = `http://${url}:${port}`;
 const resolvers = {
   Query: {
         LDAPAuthUser: (_, { email, password }) => {
-			return generalRequest(`${URL}/authuserldap`, 'GET', { email, password })
+			return generalRequest(`${URL}/auth/authuserldap`, 'GET', { email, password })
 		},
 	},
 	Mutation: {
 		LDAPCreateUser: (_,{LDAPuser}) => {
-			return generalRequest(`${URL}/createuserldap`, 'POST', LDAPuser)
+			return generalRequest(`${URL}/auth/createuserldap`, 'POST', LDAPuser)
 		},
 	}
 };
